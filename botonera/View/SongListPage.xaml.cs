a206f39a1955e7589567779c7902fcdca6f32d35
Bastián Véliz Vega <bastianx6@gmail.com>
2019-03-29 22:40:55 -0300

    Create SongListPage and transform MainPage into a TabbedPage

---
+++ botonera/View/SongListPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using botonera.Entities;
+using botonera.ViewModel;
+using Xamarin.Forms;
+
+namespace botonera.View
+{
+    public partial class SongListPage : ContentPage
+    {
+        SongListViewModel viewModel;
+
+        public SongListPage()
+        {
+            InitializeComponent();
+            viewModel = new SongListViewModel();
+            configureSongList();
+        }
+
+        private void configureSongList()
+        {
+            SongList.ItemsSource = viewModel.Songs;
+            SongList.ItemTapped += SongList_ItemTapped;
+            viewModel.UpdateSongs();
+        }
+
+
+        async void SongList_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            try
+            {
+                var song = e.Item as SongEntity;
+                var success = await viewModel.PlaySong(song.SongCode);
+                System.Diagnostics.Debug.WriteLine($"Success: {success} Song Code: {song.SongCode} | Song Description: {song.Description}");
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine($"Error on song play {ex}");
+            }
+
+        }
+    }
+}
