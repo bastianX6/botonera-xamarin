aef0cda681d5269f3874cd08a76007c7b3893be5
Bastián Véliz Vega <bastianx6@gmail.com>
2019-04-28 11:37:41 -0400

    Add FCM in iOS app

---
+++ botonera.iOS/Delegates/UserNotificationCenterDelegate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Foundation;
+using UserNotifications;
+
+namespace botonera.iOS.Delegates
+{
+    public class UserNotificationCenterDelegate: UNUserNotificationCenterDelegate
+    {
+        public UserNotificationCenterDelegate()
+        {
+        }
+
+        [Export("userNotificationCenter:willPresentNotification:withCompletionHandler:")]
+        public override void WillPresentNotification(UNUserNotificationCenter center, UNNotification notification, Action<UNNotificationPresentationOptions> completionHandler)
+        {
+            completionHandler(UNNotificationPresentationOptions.Alert | UNNotificationPresentationOptions.Sound);
+        }
+    }
+}
